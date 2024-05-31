6be72164506fb3f5717c8712ce3e9dd5c60b30e1
Tamy <tamy8677@gmail.com>
2024-05-31 17:17:04 +0800

    import Bulma, Pages controller routes views, nav bar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,11 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/assets/*"] 
+# 把vendor底下的assets資料夾下的東西也加到assets這個搜尋路徑中，方便拿來用
+# #{Rails.root}表示整個rails專案的根目錄
+# *表示資料夾內所有東西
+# 改設定檔，要重啟rails server才會生效
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
