6545fad5a6b77b95255ab2ec48af84cc02d41369
Tamy <tamy8677@gmail.com>
2024-06-03 20:04:34 +0800

    index view, user friendly id, routes, pages show user action, solve N+1 problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,76 @@
 class PagesController < ApplicationController
 
-    def index
+    def index # 全站首頁 的action
+        # @stories = Story.all # 將stories資料表中的所有資料撈出來，指定給實體變數@stories
+        @stories = Story.order(created_at: :desc).includes(:user)
+        # 將stories資料表中的所有資料撈出來由建立日期新到舊排序（不限當前使用者的文章，而是stories資料表中所有文章）
+        # vs stroy controller：@stories = current_user.stories.all.order(created_at: :desc) （當前使用者的文章）
+        # 用.includes(:user)解決N+1問題（撈資料時的SQL語法會改用IN的寫法來寫）
+
+        # 原始伺服器log顯示之查詢語法
+            # Started GET "/" for ::1 at 2024-06-03 19:56:00 +0800
+            # (0.5ms)  SELECT "schema_migrations"."version" FROM "schema_migrations" ORDER BY "schema_migrations"."version" ASC
+            # Processing by PagesController#index as HTML
+            # Rendering layout layouts/application.html.erb
+            # Rendering pages/index.html.erb within layouts/application
+            # Story Load (2.7ms)  SELECT "stories".* FROM "stories" WHERE "stories"."deleted_at" IS NULL ORDER BY "stories"."created_at" DESC
+            # ↳ app/views/pages/index.html.erb:81
+            # User Load (1.7ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # CACHE User Load (0.0ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # CACHE User Load (0.0ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # CACHE User Load (0.0ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # CACHE User Load (0.0ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # CACHE User Load (0.0ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # CACHE User Load (0.0ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # CACHE User Load (0.0ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # CACHE User Load (0.0ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/views/pages/index.html.erb:84
+            # Rendered pages/index.html.erb within layouts/application (Duration: 30.3ms | Allocations: 21626)
+            # [Webpacker] Everything's up-to-date. Nothing to do
+            # User Load (0.8ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 ORDER BY "users"."id" ASC LIMIT $2  [["id", 2], ["LIMIT", 1]]
+            # ↳ app/views/shared/frontend/_navbar.html.erb:30
+            # ActiveStorage::Attachment Load (1.9ms)  SELECT "active_storage_attachments".* FROM "active_storage_attachments" WHERE "active_storage_attachments"."record_id" = $1 AND "active_storage_attachments"."record_type" = $2 AND "active_storage_attachments"."name" = $3 LIMIT $4  [["record_id", 2], ["record_type", "User"], ["name", "avatar"], ["LIMIT", 1]]
+            # ↳ app/helpers/users_helper.rb:4:in `avatar'
+            # ActiveStorage::Blob Load (0.8ms)  SELECT "active_storage_blobs".* FROM "active_storage_blobs" WHERE "active_storage_blobs"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/helpers/users_helper.rb:4:in `avatar'
+            # Rendered shared/frontend/_navbar.html.erb (Duration: 31.0ms | Allocations: 19065)
+            # Rendered layout layouts/application.html.erb (Duration: 84.7ms | Allocations: 65443)
+            # Completed 200 OK in 107ms (Views: 61.5ms | ActiveRecord: 28.1ms | Allocations: 71810)
+
+        # 加上includes後的 伺服器log顯示之查詢語法
+            # Started GET "/" for ::1 at 2024-06-03 19:58:54 +0800
+            # Processing by PagesController#index as HTML
+            # Rendering layout layouts/application.html.erb
+            # Rendering pages/index.html.erb within layouts/application
+            # Story Load (0.7ms)  SELECT "stories".* FROM "stories" WHERE "stories"."deleted_at" IS NULL ORDER BY "stories"."created_at" DESC
+            # ↳ app/views/pages/index.html.erb:81
+            # User Load (0.5ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1  [["id", 1]]
+            # ↳ app/views/pages/index.html.erb:81
+            # Rendered pages/index.html.erb within layouts/application (Duration: 17.3ms | Allocations: 12879)
+            # [Webpacker] Everything's up-to-date. Nothing to do
+            # User Load (0.7ms)  SELECT "users".* FROM "users" WHERE "users"."id" = $1 ORDER BY "users"."id" ASC LIMIT $2  [["id", 2], ["LIMIT", 1]]
+            # ↳ app/views/shared/frontend/_navbar.html.erb:30
+            # ActiveStorage::Attachment Load (0.6ms)  SELECT "active_storage_attachments".* FROM "active_storage_attachments" WHERE "active_storage_attachments"."record_id" = $1 AND "active_storage_attachments"."record_type" = $2 AND "active_storage_attachments"."name" = $3 LIMIT $4  [["record_id", 2], ["record_type", "User"], ["name", "avatar"], ["LIMIT", 1]]
+            # ↳ app/helpers/users_helper.rb:4:in `avatar'
+            # ActiveStorage::Blob Load (0.5ms)  SELECT "active_storage_blobs".* FROM "active_storage_blobs" WHERE "active_storage_blobs"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
+            # ↳ app/helpers/users_helper.rb:4:in `avatar'
+            # Rendered shared/frontend/_navbar.html.erb (Duration: 28.9ms | Allocations: 16213)
+            # Rendered layout layouts/application.html.erb (Duration: 53.9ms | Allocations: 38985)
+            # Completed 200 OK in 59ms (Views: 37.2ms | ActiveRecord: 18.9ms | Allocations: 41670)
+    end
+
+    def show # 檢視單篇文章頁面 的 action
+    end
+
+    def user # 檢視單一作者所有文章頁面 的 action
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,17 @@ Rails.application.routes.draw do
 
   resources :stories # 做一個 文章們（符號） 相關的資源 出來 ＃複數 :複數 #複數resources長8條路徑對照7個方法 ＃單數resource的話會長7條路徑對照7個方法 不長有關id的路徑
 
+  # 某一使用者 的 某一篇文章 頁面路徑
+  # /@使用者名稱/文章標題-123
+  get '@:username/:story_id', to: 'pages#show', as: 'story_page'
+  # 當網址於@:username/:story_id，且使用get方法，就使用 pages controller的show action
+  # prefix前綴 為story_page
+  #  story_page  GET   /@:username/:story_id(.:format)      pages#show
+
+  # 單一使用者的所有文章 頁面路徑
+  get '@:username/', to: 'pages#user', as: 'user_page'
+  #  user_page  GET    /@:username(.:format)      pages#user
+
   root 'pages#index'
   # 將首頁設定至 pages controller中的 index action
 end
