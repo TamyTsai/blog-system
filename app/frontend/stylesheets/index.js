6b81fcc52bc8833d5e684472a60c56e619f7405b
Tamy <tamy8677@gmail.com>
2024-06-05 15:03:47 +0800

    import font awesome, clap view

--- app/frontend/stylesheets/index.js
+++ app/frontend/stylesheets/index.js
@@ -1,4 +1,9 @@
 import 'bulma'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+// yarn add @fortawesome/fontawesome-free
+// 安裝完fontawesome 還要引入才有效果
+// min是壓縮過的檔案 比較小（沒空格、換行）
+// 安裝引入後要重新啟動伺服器
 
 import './application.scss'
 // .表示這個資料夾
