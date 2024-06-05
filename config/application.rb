d1fa8463e538042940092aa85b9490358f0917cd
Tamy <tamy8677@gmail.com>
2024-06-05 09:22:33 +0800

    update generator config, delete test folder

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,16 @@ module Blog
     # 改設定檔後 要重新啟動伺服器 才會生效
     # 2024-06-01 20:23:03 +0800
 
+    config.generators do |g| # 參數不一定要取g，但取什麼，程式碼區塊裡的就要寫得一樣
+      g.assets false
+      g.helper false
+      g.test_framework false # 測試框架：本專案沒寫測試，所以可以關掉
+    end
+    # 如果assets目錄下的東西都要交給webpack打包，就不需要在使用rails generate時，把檔案產生在原assets目錄下（這邊設定不自動產生，要產生就自己建立）
+    # 建立cotroller或model時，就不會再把檔案產生在以上目錄
+    
+    # 可手動將測試資料夾（test）刪除
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
