0e1b144aa9e7cced95173e3ddd1cfe5d89e7912e
Tamy <tamy8677@gmail.com>
2024-06-09 22:48:40 +0800

    hamburger menu js

--- app/assets/packs/application.js
+++ app/assets/packs/application.js
@@ -1,32 +1,45 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// 這三個目錄（app lib vendor 下的 assets），在預設情況下這三個資料夾的東西是共通的(因為都會被打包成一個檔案)
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// 其中的require jquery和require jquery_ujs會載入JQuery和Rails的JQuery adapater，這是因為我們在Gemfile中有裝jquery-rails這個套件，所以這裡可以讀取的到。
+// require turbolinks在上一章有提到。而require_tree .會載入這個目錄下的所有JavaScript檔案。
+// 總之，這個manifest的最後輸出結果就是通通壓縮成一個application.js檔案。
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
 
-import "controllers"
-// 會去找javascript > controllers > index.js
-// 另外一個叫stimulus的框架
 
-// import 'bulma'
-// 會到node_modules中找相對應的模組出來
-// 在此js檔案 import了一個css
-// bulma css 被打包到js中
-// 對webpacker來說，所有圖片、字型、css等 都是js
-// bulma跟樣式有關，所以新開一個檔案去引入比較好（可一眼看出檔案中所要import的東西都跟樣式有關，不是js）
+// 漢堡選單js
+document.addEventListener('DOMContentLoaded', () => {
 
-import '../stylesheets'
-// ..表示上一層資料夾
-// 如果沒有明確寫到檔案，只有寫到目錄，js慣例會去找該路徑資料夾下名為index的檔案
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-// stories.scss 被引進 application.scss 被引進 index.js
-// bulma 被引進 index.js
-// application.js 引入上面所有
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => { //ES6箭頭函式寫法
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+});
