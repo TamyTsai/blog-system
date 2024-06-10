34f3f12a2e96f0fb0ce48f1d54beb992fb147340
Tamy <tamy8677@gmail.com>
2024-06-10 17:13:02 +0800

    hamburger menu compiled by Sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,15 +21,15 @@
 // 總之，這個manifest的最後輸出結果就是通通壓縮成一個application.js檔案。
 
 
-
-// 漢堡選單js
-document.addEventListener('DOMContentLoaded', () => {
+// Bulma漢堡選單js
+// https://bulma.io/documentation/components/navbar/
+document.addEventListener('turbolinks:load', () => { // Rails 6 中Turbo links預設為開啟 所以如果用 DOMContentLoaded 可能沒辦法間聽到正確之事件，要改用turbolinks:load
 
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
   // Add a click event on each of them
-  $navbarBurgers.forEach( el => { //ES6箭頭函式寫法
+  $navbarBurgers.forEach( el => {
     el.addEventListener('click', () => {
 
       // Get the target from the "data-target" attribute
@@ -40,6 +40,18 @@ document.addEventListener('DOMContentLoaded', () => {
       el.classList.toggle('is-active');
       $target.classList.toggle('is-active');
 
+    });
+  });
+
+});
+
+// 提醒視窗叉叉功能
+document.addEventListener('turbolinks:load', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
     });
   });
 });
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -30,3 +30,38 @@ import '../stylesheets'
 // stories.scss 被引進 application.scss 被引進 index.js
 // bulma 被引進 index.js
 // application.js 引入上面所有
+
+// Bulma漢堡選單js
+// https://bulma.io/documentation/components/navbar/
+document.addEventListener('turbolinks:load', () => { // Rails 6 中Turbo links預設為開啟 所以如果用 DOMContentLoaded 可能沒辦法間聽到正確之事件，要改用turbolinks:load
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
+    });
+  
+  });
+  
+  // 提醒視窗叉叉功能
+  document.addEventListener('turbolinks:load', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      const $notification = $delete.parentNode;
+  
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
+  });
